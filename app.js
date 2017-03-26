5efe25e2118f7bc864c039d145a79f77732391fd
mobilemelody <melody.reebs@gmail.com>
2017-03-25 21:27:02 -0700

    Updated database variable

--- app.js
+++ app.js
@@ -10,13 +10,8 @@ var mongoose = require('mongoose');
 var Task = require('./models/Tasks');
 var Comment = require('./models/Comments');
 
-// Here we find an appropriate database to connect to, defaulting to localhost if we don't find one.
-var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb:localhost/list';
+var uristring = process.env.MONGODB_URI;
 
-// The http server will listen to an appropriate port, or default to port 5000.
-var theport = process.env.PORT || 5000;
-
-// Makes connection asynchronously.  Mongoose will queue up database operations and release them when the connection is complete.
 mongoose.connect(uristring, function(err, res) {
 	if(err) {
 		console.log('ERROR connecting to: ' + uristring + '. ' + err);
